7640a29245b89c0c5a6f24355efa93b72fb8c26b
Jamie Brynes <jamiebrynes7@gmail.com>
2019-08-21 23:42:38 +0100

    should work!

--- .github/actions/build-rust-musl/entrypoint.sh
+++ .github/actions/build-rust-musl/entrypoint.sh
@@ -18,4 +18,4 @@ echo "----> Building Rust binary"
 cargo build --release --target x86_64-unknown-linux-musl
 
 echo "----> Build is complete. Zipping result."
-zip -j bootstrap.zip ./x86_64-unknown-linux-musl/release/bootstrap
+zip -j bootstrap.zip ./target/x86_64-unknown-linux-musl/release/bootstrap
