0f35e6374ce112f857f7629fc6bbbe915b30de75
Jamie Brynes <jamiebrynes7@gmail.com>
2019-08-21 22:35:52 +0100

    build action stub

---
+++ .github/actions/build-rust-musl/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z "${GITHUB_WORKSPACE}" ]]; then
+  echo "Set the GITHUB_WORKSPACE env variable."
+  exit 1
+fi
+
+if [[ -z "${GITHUB_REPOSITORY}" ]]; then
+  echo "Set the GITHUB_REPOSITORY env variable."
+  exit 1
+fi
+
+cd "${GITHUB_WORKSPACE}"
+
+echo "----> Building Rust binary"
+cargo build --release 
+
+echo "----> Build is complete. Zipping result."
+zip -j bootstrap.zip ./x86_64-unknown-linux-musl/release/bootstrap
