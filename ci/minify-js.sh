78d3c37f0f2203a10e63792b0b22f6b44dc4a0a8
Jamie Brynes <jamiebrynes7@gmail.com>
2019-08-22 23:04:51 +0100

    it works!

--- ci/minify-js.sh
+++ ci/minify-js.sh
@@ -6,7 +6,8 @@ function minify()
 {
     filename=$1
     min_filename="./static/${filename%.js}.min.js"
-    curl -X POST -s --data-urlencode `cat ${filename}` https://javascript-minifier.com/raw > "${min_filename}"
+    echo "****** Minifying ${filename} ----> ${min_filename} ******"
+    curl -X POST -s --data-urlencode "input=`cat ${filename}`" https://javascript-minifier.com/raw > "${min_filename}"
 }
 
 for filename in ./js/*.js; do
---
+++ static/js/nav.min.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",()=>{const e=Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"),0);e.length>0&&e.forEach(e=>{e.addEventListener("click",()=>{const t=e.dataset.target,a=document.getElementById(t);e.classList.toggle("is-active"),a.classList.toggle("is-active")})})});
