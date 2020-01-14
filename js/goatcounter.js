d83a2e866e5043bf02eb18d87b1c8da03502c05a
Jamie Brynes <jamiebrynes7@gmail.com>
2020-01-14 23:16:35 +0000

    Add basic analytics (#27)

---
+++ js/goatcounter.js
@@ -0,0 +1,13 @@
+(function() {
+    if (window.location.host != "jamiebrynes.com") {
+        return;
+    }
+    
+    var script = document.createElement('script');
+    window.counter = 'https://jamiebrynes_com.goatcounter.com/count'
+    script.async = 1;
+    script.src = '//gc.zgo.at/count.js';
+
+    var ins = document.getElementsByTagName('script')[0];
+    ins.parentNode.insertBefore(script, ins)
+})();
