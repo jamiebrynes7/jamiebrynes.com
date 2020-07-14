435f2063c8929b8099b82b7e42d7fafccca77cf9
Jamie Brynes <jamiebrynes7@gmail.com>
2020-07-14 19:47:53 +0100

    purge css

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -7,3 +7,5 @@ fi
 npm run build-css
 
 curl -sL https://github.com/getzola/zola/releases/download/v0.11.0/zola-v0.11.0-x86_64-unknown-linux-gnu.tar.gz | tar zxv && ./zola build --base-url ${DEPLOY_URL}
+
+npm run purge-cs
