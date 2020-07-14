971ebb3c6311e6e7aacd5a6c39dfe286f8783f51
Jamie Brynes <jamiebrynes7@gmail.com>
2020-07-14 19:54:29 +0100

    supposedly this works

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,8 +4,8 @@ if [[ "${CONTEXT}" == "production" ]]; then
     DEPLOY_URL="https://www.jamiebrynes.com"
 fi
 
+NODE_ENV=production
+
 npm run build-css
 
 curl -sL https://github.com/getzola/zola/releases/download/v0.11.0/zola-v0.11.0-x86_64-unknown-linux-gnu.tar.gz | tar zxv && ./zola build --base-url ${DEPLOY_URL}
-
-npm run purge-css
