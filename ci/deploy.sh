db1eb8454ef3224a26c07630032636e2860aaee6
Jamie Brynes <jamiebrynes7@gmail.com>
2020-07-14 19:40:35 +0100

    update deploy script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,4 +4,6 @@ if [[ "${CONTEXT}" == "production" ]]; then
     DEPLOY_URL="https://www.jamiebrynes.com"
 fi
 
-curl -sL https://github.com/getzola/zola/releases/download/v0.7.0/zola-v0.7.0-x86_64-unknown-linux-gnu.tar.gz | tar zxv && ./zola build --base-url ${DEPLOY_URL}
+npm run build-css
+
+curl -sL https://github.com/getzola/zola/releases/download/v0.11.0/zola-v0.11.0-x86_64-unknown-linux-gnu.tar.gz | tar zxv && ./zola build --base-url ${DEPLOY_URL}
