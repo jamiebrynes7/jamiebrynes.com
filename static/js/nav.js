e561c825eb3662c251c1aa7997daae538c00a78d
Jamie Brynes <jamiebrynes7@gmail.com>
2020-07-22 20:58:17 +0100

    better nav friendly

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,15 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const navbarBurger = document.getElementById("nav-toggle");
+  const navbarBurgerIcon = document.getElementById("nav-hamburger");
+  const navCrossIcon = document.getElementById("nav-cross");
 
   navbarBurger.addEventListener('click', () => {
-  
-    // Get the target from the "data-target" attribute
-    const target = navbarBurger.dataset.target;
-    const $target = document.getElementById(target);
+    const target = document.getElementById(navbarBurger.dataset.target);
 
-    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    navbarBurger.classList.toggle('hidden');
-    $target.classList.toggle('hidden');
+    navbarBurgerIcon.classList.toggle('hidden');
+    navCrossIcon.classList.toggle('hidden');
+    target.classList.toggle('hidden');
   });
 });
