3de7657aa5e2572754e9d9ee92c0d53ff43b55c6
Jamie Brynes <jamiebrynes7@gmail.com>
2021-01-18 19:29:05 +0000

    Remove all the old stuff

--- ci/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ "${CONTEXT}" == "production" ]]; then
-    DEPLOY_URL="https://www.jamiebrynes.com"
-fi
-
-curl -sL https://github.com/getzola/zola/releases/download/v0.7.0/zola-v0.7.0-x86_64-unknown-linux-gnu.tar.gz | tar zxv && ./zola build --base-url ${DEPLOY_URL}
