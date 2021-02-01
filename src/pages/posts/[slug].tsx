45ffd44a7570e566af5b96a8b4d049c582336b42
Jamie Brynes <jamiebrynes7@gmail.com>
2021-02-01 22:48:46 +0000

    add email & twitter intents

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,4 +1,7 @@
 import PageContainer from "@components/PageContainer";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import dayjs from "dayjs";
 import { readdirSync } from "fs";
 import { GetStaticPaths, GetStaticProps } from "next";
@@ -7,16 +10,49 @@ import { getPostBySlug } from "src/data";
 
 const Post: React.FC<Props> = ({ slug }) => {
   const { metadata, component: Component } = getPostBySlug(slug);
+  const url = `https://jamiebrynes.com/posts/${slug}`;
+  const twitterIntent = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+    metadata.title
+  )}&url=${encodeURIComponent(url)}&via=jamiebrynes`;
+
+  const emailIntent = `mailto:?subject=${encodeURIComponent(
+    metadata.title
+  )}&body=${encodeURIComponent(url)}`;
   return (
     <PageContainer title={metadata.title}>
       <div className="divide-y xl:divide-y-0 divide-gray-200 xl:grid xl:grid-cols-5 xl:gap-x-6 pb-16 xl:pb-20">
-        <div className="pt-6 pb-10 xl:pt-11 xl:border-b xl:border-gray-200">
-          <p className="text-base font-semibold text-gray-700 mb-1">
-            Published on:
-          </p>
-          <p className="text-gray-500">
-            {dayjs.unix(metadata.date).format("MMM DD, YYYY")}
-          </p>
+        <div>
+          <div className="pt-6 pb-6 pl-2 xl:border-b xl:border-gray-200">
+            <p className="text-base font-semibold text-gray-700 mb-1">
+              Published on:
+            </p>
+            <p className="text-gray-500">
+              {dayjs.unix(metadata.date).format("MMM DD, YYYY")}
+            </p>
+          </div>
+          <div className="pt-6 pb-6 pl-2 xl:border-b xl:border-gray-200">
+            <p className="text-base font-semibold text-gray-700 mb-1">
+              Share on:
+            </p>
+            <span className="text-gray-500 space-x-4">
+              <a
+                className="hover:text-blue-500"
+                href={twitterIntent}
+                target="_blank"
+                rel="noopener"
+              >
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+              <a
+                className="hover:text-blue-500"
+                href={emailIntent}
+                target="_blank"
+                rel="noopener"
+              >
+                <FontAwesomeIcon icon={faEnvelope} />
+              </a>
+            </span>
+          </div>
         </div>
         <div className="divide-y divide-gray-200 xl:pb-0 xl:col-span-4 xl:row-span-2">
           <div className="prose max-w-none pt-10 pb-8">
