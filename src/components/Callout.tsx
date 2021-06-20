6e0b6c8268d244b34145b4ed5701a29aa43ba8c0
Jamie Brynes <jamiebrynes7@gmail.com>
2021-06-20 18:21:53 +0100

    update callout component

--- src/components/Callout.tsx
+++ src/components/Callout.tsx
@@ -1,3 +1,9 @@
+import {
+  faExclamationCircle,
+  faInfoCircle,
+  faTimesCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ReactElement } from "react";
 
 const Callout: React.FC<{ type: "info" | "warn" | "danger" }> = ({
@@ -11,31 +17,27 @@ const Callout: React.FC<{ type: "info" | "warn" | "danger" }> = ({
 
   switch (type) {
     case "info":
-      bgColor = "bg-blue-100 dark:bg-blue-900";
-      borderColor = "border-blue-500 dark:border-blue-700";
-      iconColor = "text-blue-900 dark:text-blue-300";
-      icon = <InfoIcon />;
+      bgColor = "bg-blue-100 dark:bg-blue-800";
+      borderColor = "border-blue-500 dark:border-blue-400";
+      iconColor = "text-blue-500 dark:text-blue-400";
+      icon = <FontAwesomeIcon icon={faInfoCircle} size="lg" />;
       break;
     case "warn":
-      bgColor = "bg-yellow-100 dark:bg-yellow-900";
-      borderColor = "border-yellow-500 dark:border-yellow-700";
-      iconColor = "text-yellow-900 dark:text-yellow-300";
-      icon = <WarningIcon />;
+      bgColor = "bg-yellow-100 dark:bg-yellow-800";
+      borderColor = "border-yellow-500 dark:border-yellow-400";
+      iconColor = "text-yellow-500 dark:text-yellow-300";
+      icon = <FontAwesomeIcon icon={faExclamationCircle} size="lg" />;
       break;
     case "danger":
-      bgColor = "bg-red-100 dark:bg-red-900";
-      borderColor = "border-red-500 dark:border-red-700";
-      iconColor = "text-red-900 dark:text-red-300";
-      icon = <DangerIcon />;
+      bgColor = "bg-red-100 dark:bg-red-800";
+      borderColor = "border-red-500 dark:border-red-400";
+      iconColor = "text-red-500 dark:text-red-400";
+      icon = <FontAwesomeIcon icon={faTimesCircle} size="lg" />;
       break;
   }
 
   return (
-    <div
-      className={
-        "flex px-4 py-2 rounded-lg border " + bgColor + " " + borderColor
-      }
-    >
+    <div className={"flex px-4 py-2 border-l-4 " + bgColor + " " + borderColor}>
       <div className={"mr-3 mt-5 " + iconColor}>{icon}</div>
       <div className="dark:text-gray-300">{children}</div>
     </div>
@@ -43,63 +45,3 @@ const Callout: React.FC<{ type: "info" | "warn" | "danger" }> = ({
 };
 
 export default Callout;
-
-const InfoIcon: React.FC<{}> = () => {
-  return (
-    <svg
-      width="24px"
-      height="24px"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-      />
-    </svg>
-  );
-};
-
-const WarningIcon: React.FC<{}> = () => {
-  return (
-    <svg
-      width="24px"
-      height="24px"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-      />
-    </svg>
-  );
-};
-
-const DangerIcon: React.FC<{}> = () => {
-  return (
-    <svg
-      width="24px"
-      height="24px"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
-      />
-    </svg>
-  );
-};
