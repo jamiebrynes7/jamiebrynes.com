a4e2d69c5d278602002f30e994d2008c1ce943e9
Jamie Brynes <jamiebrynes7@gmail.com>
2023-09-18 23:23:46 +0000

    [nix] swap to standalone tailwindcss, add package for building site

--- flake.nix
+++ flake.nix
@@ -1,16 +1,38 @@
 {
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
-
-  outputs = { self, nixpkgs }:
-  let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    devShell.x86_64-linux =
-      pkgs.mkShell {
-        buildInputs = [
-          pkgs.hugo
-          pkgs.nodejs
-        ];
-      };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.website = pkgs.stdenv.mkDerivation rec {
+          pname = "jamiebrynes.com";
+          version = "1.0.0";
+          src = ./.;
+          nativeBuildInputs = [ 
+            pkgs.hugo
+            pkgs.tailwindcss
+          ];
+          buildPhase = ''
+            tailwindcss -i assets/css/main.css -o static/main.min.css --minify
+            hugo --minify
+          '';
+          installPhase = ''
+            cp -r public $out
+          '';
+        };
+        defaultPackage = self.packages.${system}.website;
+
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.hugo
+            pkgs.tailwindcss
+          ];
+        };
+      }
+    );
 }
